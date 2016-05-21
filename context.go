d56d62fba6780b8f87c40030cf9d894c799c00c1
Ben Guild <benguild@users.noreply.github.com>
2016-05-21 21:51:00 +0900

    First commit.

---
+++ context.go
@@ -0,0 +1,24 @@
+package gaebridge
+
+// Only creates and returns a single Google App Engine (GAE) context, once per request.
+
+import (
+	"google.golang.org/appengine"
+	"golang.org/x/net/context"
+	"net/http"
+)
+
+var (
+	ctx *context.Context
+
+)
+
+func Context(r *http.Request) *context.Context {
+	if (ctx==nil) {
+		ctx = &appengine.NewContext(r);
+
+	}
+
+	return ctx;
+
+}
---
+++ debuglogger.go
@@ -0,0 +1,28 @@
+package gaebridge
+
+// Provides standard debug logging functionality by wrapping Google App Engine (GAE)'s logging methods.
+
+/*
+For example, this can be used to log GORM's database queries:
+
+```
+db.LogMode(true);
+db.SetLogger(log.New(gaebridge.DebugLogWriter{ gaebridge.Context(c) }, "", log.LstdFlags));
+```
+ */
+
+import (
+	"golang.org/x/net/context"
+	"google.golang.org/appengine/log"
+)
+
+type DebugLogWriter struct {
+	Context *context.Context
+
+}
+
+func (w DebugLogWriter) Write(p []byte) (n int, err error) {
+	log.Debugf(*w.Context, string(p));
+	return len(p), nil;
+
+}
