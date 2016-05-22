0ebf173315f27f63262b6e54fd9fcfe94d49df56
pjebs <enquiries@pjebs.com.au>
2016-05-22 19:41:53 +1000

    Refactored code
    - Not tested (or even compiled).

---
+++ log/debuglogger.go
@@ -0,0 +1,38 @@
+// +build appengine
+
+package log
+
+// Provides standard debug logging functionality by wrapping Google App Engine (GAE)'s logging methods.
+
+/*
+For example, this can be used to log GORM's database queries:
+
+```
+db.LogMode(true)
+db.SetLogger(log.NewDebugLogWriter(context.Context(r)))
+```
+*/
+
+import (
+	"fmt"
+	"golang.org/x/net/context"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
+	"log"
+)
+
+type DebugLogWriter struct {
+	context *context.Context
+}
+
+func NewDebugLogWriter(ctx *context.Context) *DebugLogWriter {
+	return &DebugLogWriter{context: ctx}
+}
+
+func (w DebugLogWriter) Print(v ...interface{}) {
+	if !appengine.IsDevAppServer() {
+		log.Debugf(*w.Context, fmt.Sprint(v))
+	} else {
+		log.Print(v)
+	}
+}
